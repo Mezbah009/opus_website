19dc2d701bd5b1c2bc66b7cd0f2c7d8af732e531
Md.Habibullah Mezbah <mezba@opus-bd.com>
2024-04-24 10:45:55 +0600

    testimonial forntend

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -419,6 +419,66 @@
     </div>
 </section><!-- End Frequently Asked Questions Section -->
 
+<!-- Clients Aside -->
+<section id="partner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title text-center">
+                    <h3>They Trust Us</h3>
+                    {{-- <p>Duis aute irure dolor in reprehenderit in voluptate</p> --}}
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="clients">
+                @foreach($clients as $client)
+
+                <div class="col-md-12">
+                    <a href="{{ $client->link }}" target="_blank">
+                    <img src="/uploads/first_section/{{ $client->logo }}" alt="..." width="150px">
+                    </a>
+                </div>
+                @endforeach
+            </div>
+
+        </div>
+    </div>
+</section> <!-- End Clients Section -->
+
+
+
+<!-- Start Testimonial Section -->
+<section  class="faq section-bg">
+<div id="testimonial" class="testimonial-section">
+    <div class="container">
+
+
+        <!-- Start Testimonials Carousel -->
+        <div id="testimonial-carousel" class="testimonials-carousel">
+            @foreach ($testimonials as $key => $testimonial)
+            <!-- Testimonial 1 -->
+            <div class="testimonials item">
+                <div class="testimonial-content">
+                    <img src="/uploads/first_section/{{ $testimonial->logo }}" alt="" >
+                    <div class="testimonial-author">
+                        <div class="author">{{$testimonial->name}}</div>
+                        <div class="designation">{{$testimonial->designation}}</div>
+                    </div>
+                    <p>{{$testimonial->description}}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+        <!-- End Testimonials Carousel -->
+    </div>
+</div>
+</section>
+<!-- End Testimonial Section -->
+
+
 <!-- ======= Contact Us Section ======= -->
 <section id="contact" class="contact">
     <div class="container" data-aos="fade-up">
@@ -490,80 +550,6 @@
     </div>
 </section><!-- End Contact Us Section -->
 
-
-<!-- Start Testimonial Section -->
-<section  class="faq section-bg">
-<div id="testimonial" class="testimonial-section">
-    <div class="container">
-        <!-- Start Testimonials Carousel -->
-        <div id="testimonial-carousel" class="testimonials-carousel">
-            <!-- Testimonial 1 -->
-            <div class="testimonials item">
-                <div class="testimonial-content">
-                    <img src="front-assets/img/testimonial/face_1.png" alt="" >
-                    <div class="testimonial-author">
-                        <div class="author">John Doe</div>
-                        <div class="designation">Organization Founder</div>
-                    </div>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque<br> laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
-                </div>
-            </div>
-            <!-- Testimonial 2 -->
-            <div class="testimonials item">
-                <div class="testimonial-content">
-                    <img src="front-assets/img/testimonial/face_2.png" alt="" >
-                    <div class="testimonial-author">
-                        <div class="author">Jane Doe</div>
-                        <div class="designation">Lead Developer</div>
-                    </div>
-                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia<br> consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-                </div>
-            </div>
-            <!-- Testimonial 3 -->
-            <div class="testimonials item">
-                <div class="testimonial-content">
-                    <img src="front-assets/img/testimonial/face_3.png" alt="" >
-                    <div class="testimonial-author">
-                        <div class="author">John Doe</div>
-                        <div class="designation">Honorable Customer</div>
-                    </div>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit<br> anim laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
-        </div>
-        <!-- End Testimonials Carousel -->
-    </div>
-</div>
-</section>
-<!-- End Testimonial Section -->
-
-<!-- Clients Aside -->
-<section id="partner">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-title text-center">
-                    <h3>They Trust Us</h3>
-                    {{-- <p>Duis aute irure dolor in reprehenderit in voluptate</p> --}}
-                </div>
-            </div>
-        </div>
-        <div class="row">
-
-            <div class="clients">
-                @foreach($clients as $client)
-
-                <div class="col-md-12">
-                    <a href="{{ $client->link }}" target="_blank">
-                    <img src="/uploads/first_section/{{ $client->logo }}" alt="..." width="150px">
-                    </a>
-                </div>
-                @endforeach
-            </div>
-
-        </div>
-    </div>
-</section>
 
 
 @endsection
