f3e83eb7065e446a6f48b6d2f4937f287b2eb7a2
Md.Habibullah Mezbah <mezba@opus-bd.com>
2024-04-25 17:46:57 +0600

    footer

--- resources/views/admin/numbers/list.blade.php
+++ resources/views/admin/numbers/list.blade.php
@@ -4,10 +4,10 @@
     <div class="container-fluid my-2">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Contact Us</h1>
+                <h1>Numbers And Email</h1>
             </div>
             <div class="col-sm-6 text-right">
-                <a href="{{route('contact.create')}}" class="btn btn-primary">New Contact Us</a>
+                <a href="{{route('numbers.create')}}" class="btn btn-primary">New Number & Email</a>
             </div>
         </div>
     </div>
@@ -22,7 +22,7 @@
             <form action="" method="GET">
                 <div class="card-header">
                     <div class="card-title">
-                        <button type="button" onclick="window.location.href='{{route("contact.index")}}'"
+                        <button type="button" onclick="window.location.href='{{route("numbers.index")}}'"
                             class="btn btn-default btn-sm">reset</button>
                     </div>
                     <div class="card-tools">
@@ -44,11 +44,8 @@
                     <thead>
                         <tr>
                             <th width="60">ID</th>
-                            <th>Image</th>
-                            <th>Flag</th>
-                            <th>Country Name</th>
-                            <th>Company Name</th>
-                            <th>Office Name</th>
+                            <th>Numbers</th>
+                            <th>Email</th>
                             <th width="100">Action</th>
                         </tr>
                     </thead>
@@ -57,27 +54,8 @@
                         @foreach ($sections as $section)
                         <tr>
                             <td>{{$section->id}}</td>
-                            <td>
-                                @if(!empty($section->image))
-                                <img src="{{asset('uploads/first_section/'.$section->image)}}" class="img-thumbnail"
-                                    alt="{{$section->title}}" width="50">
-                                @else
-                                <img src="{{asset('admin-assets/img/default.png')}}" class="img-thumbnail"
-                                    alt="default image" width="50">
-                                @endif
-                            </td>
-                            <td>
-                                @if(!empty($section->flag))
-                                <img src="{{asset('uploads/first_section/'.$section->flag)}}" class="img-thumbnail"
-                                    alt="{{$section->title}}" width="50">
-                                @else
-                                <img src="{{asset('admin-assets/img/default.png')}}" class="img-thumbnail"
-                                    alt="default image" width="50">
-                                @endif
-                            </td>
-                            <td>{{$section->country_name}}</td>
-                            <td>{{$section->company_name}}</td>
-                            <td>{{$section->office_name}}</td>
+                            <td>{{$section->email}}</td>
+                            <td>{{$section->phone}}</td>
                             {{-- <td>
                                 @if ($section->active == 'Yes')
                                 <svg class="text-success-500 h-6 w-6 text-success" xmlns="http://www.w3.org/2000/svg"
@@ -95,7 +73,7 @@
                                 @endif
                             </td> --}}
                             <td>
-                                <a href="{{route('contact.edit',$section->id)}}">
+                                <a href="{{route('numbers.edit',$section->id)}}">
                                     <svg class="filament-link-icon w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path
@@ -133,7 +111,7 @@
 @section('customJs')
 <script>
     function destroySection(id){
-            var url = '{{ route("contact.delete", "ID") }}';
+            var url = '{{ route("numbers.delete", "ID") }}';
 
             var newUrl  = url.replace("ID",id)
             if (confirm("Are you sure you want to delete")) {
@@ -149,7 +127,7 @@
                         // Handle success response here
                         $("button[type=submit]").prop('disabled', false);
                         if (response["status"]) {
-                            window.location.href = "{{route('contact.index')}}";
+                            window.location.href = "{{route('numbers.index')}}";
                         } else {
                             // Handle other cases if needed
                         }
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -119,69 +119,42 @@
         <div class="footer-top">
             <div class="container">
                 <div class="row">
-
-                    <div class="col-lg-3 col-md-6 footer-info">
-                        <h3>Opus</h3>
-                        <p>
-                            Khan Tower (2nd Floor & 4th Floor)
-                            80/3, VIP Road, Kakrail
-                            Dhaka, Bangladesh.<br><br>
-                            <strong>Phone:</strong>+8801811317129<br>
-                            <strong>Email:</strong> info@opus-bd.com<br>
-                        </p>
-                        <div class="social-links mt-3">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                    <div class="row" style="text-align: center">
+                        @foreach($contacts as $contact)
+                        <div class="col-lg-4 d-flex" data-aos="fade-up">
+                            <div class="info-box">
+                                <img src="/uploads/first_section/{{ $contact->image }}" alt="..." width="150px">
+                                <h3>{{ $contact->country_name }}</h3>
+                                <h5>{{ $contact->company_name }}</h5>
+                                <h6>{{ $contact->office_name }}</h6>
+                                <p>{{ $contact->address }}</p>
+                                <div class="social-links mt-3">
+                                    <a href="{{ $contact->website }}" class="twitter"><i
+                                            class="bx bxl-internet-explorer"></i></a>
+                                    <a href="{{ $contact->facebook }}" class="facebook"><i class="bx bxl-facebook"></i></a>
+                                    <a href="{{ $contact->youtube }}" class="instagram"><i class="bx bxl-youtube"></i></a>
+                                    <a href="{{ $contact->linkedIn }}" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                </div>
+                            </div>
                         </div>
+                        @endforeach
                     </div>
+                </div>
+                <br>
+                <br>
+                <div style="text-align: center">
+                    @foreach($numbers as $number)
+                    <p>{{ $number->phone }}</p>
 
-                    <div class="col-lg-2 col-md-6 footer-links">
-                        <h4>Useful Links</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-                        </ul>
-                    </div>
+                    <p>{{ $number->email }}</p>
 
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Services</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 footer-newsletter">
-                        <h4>Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-                        <form action="" method="post">
-                            <input type="email" name="email"><input type="submit" value="Subscribe">
-                        </form>
-
-                    </div>
-
+                    @endforeach
                 </div>
             </div>
         </div>
-
         <div class="container">
             <div class="copyright">
                 &copy; Copyright <strong><span>Opus</span></strong>. All Rights Reserved
-            </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/ -->
                 Designed by <a href="https://opus-bd.com/">Opus Technology Limited</a>
             </div>
         </div>
