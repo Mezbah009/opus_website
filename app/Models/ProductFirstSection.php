9aec7da8600d2cf70094826c3f9d4bcda005e6c7
Md.Habibullah Mezbah <mezba@opus-bd.com>
2024-04-30 11:05:07 +0600

    Product first section

--- app/Models/ProductFirstSection.php
+++ app/Models/ProductFirstSection.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class ProductFirstSection extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'image', 'logo', 'brochure', 'product_id'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
